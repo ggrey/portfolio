4eec5f1cc1e080b7bd19b367dc239b04c18c2628
Georgina <ggrey@18techs.com>
2018-06-19 10:25:41 -0600

    Include dependencies Add firebase connection file

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+require('dotenv').config();
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "grey-contact.firebaseapp.com",
+    databaseURL: "https://grey-contact.firebaseio.com"
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+export default base;
